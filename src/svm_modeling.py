35494c5cda520c8738b092ea0311681fb6e67a95
Kamil A. Kaczmarek <kamil.kaczmarek@neptune.ml>
2022-07-05 23:17:36 +0200

    svm modeling (#1)
    * added svm modeling, utils and env definition
    
    * svm part ready
    
    * added more relevant comments
    
    * updated env

---
+++ src/svm_modeling.py
@@ -0,0 +1,85 @@
+import neptune.new as neptune
+import pandas as pd
+from sklearn import svm
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+from utils import log_dataset, log_raw_data, log_training_report, log_pca
+
+DATA_PATH = "../data/covid_and_healthy_spectra.csv"
+
+run = neptune.init_run(
+    project="kamil/analysis", source_files=["*.py", "../environment.yml"], tags="svm"
+)
+
+# configuration
+config = {
+    "test_size": 0.15,
+    "val_size": 0.50,
+    "n_components": 2,
+    "scaler": False,
+    "seed": 2022,
+    "column_select": False,
+    "nth_column": 10,
+    "log_model": True,
+}
+
+run["config"] = config
+
+# data management
+df = pd.read_csv(DATA_PATH)
+
+# (neptune) log data version, data dimensions, target occurrences
+log_raw_data(run=run, base_namespace="data/raw", df=df)
+
+if config["column_select"]:
+    df = df[df.columns[:: config["nth_column"]]]
+
+df.diagnostic = df.diagnostic.apply(lambda x: 1 if x == "SARS-CoV-2" else 0)
+
+y = df.diagnostic
+X = df[df.columns.drop("diagnostic")]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=config["test_size"], random_state=config["seed"]
+)
+X_test, X_val, y_test, y_val = train_test_split(
+    X_test, y_test, test_size=config["val_size"], random_state=config["seed"]
+)
+
+# modeling pipeline, feature management, metrics reporting
+if config["scaler"]:
+    scaler = MinMaxScaler()
+    scaler.fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+    X_val = scaler.transform(X_val)
+
+pca = PCA(n_components=config["n_components"])
+pca.fit(X_train)
+X_train = pca.transform(X_train)
+X_test = pca.transform(X_test)
+X_val = pca.transform(X_val)
+
+# (neptune) log PCA results
+log_pca(run=run, base_namespace="data/pca", pca=pca)
+
+# (neptune) log metadata for train, valid, test
+log_dataset(run=run, base_namespace="data/train", data=X_train, target=y_train)
+log_dataset(run=run, base_namespace="data/valid", data=X_val, target=y_val)
+log_dataset(run=run, base_namespace="data/test", data=X_test, target=y_test)
+
+
+clf = svm.SVC()
+clf.fit(X_train, y_train)
+
+y_train_pred = clf.predict(X_train)
+y_val_pred = clf.predict(X_val)
+y_test_pred = clf.predict(X_test)
+
+y_data = zip((y_train, y_val, y_test), (y_train_pred, y_val_pred, y_test_pred))
+
+log_training_report(run=run, base_namespace="modeling", y_data=y_data)
+
+if config["log_model"]:
+    run["modeling/pickled_model"] = neptune.types.File.as_pickle(clf)
---
+++ src/utils.py
@@ -0,0 +1,70 @@
+import neptune.new as neptune
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import sklearn.decomposition
+from sklearn.metrics import precision_score, accuracy_score, recall_score
+
+
+def log_raw_data(run: neptune.Run, base_namespace: str, df: pd.DataFrame):
+    run[f"{base_namespace}/version"].track_files(
+        "../data/covid_and_healthy_spectra.csv"
+    )
+
+    run[f"{base_namespace}/n_rows"] = df.shape[0]
+    run[f"{base_namespace}/n_cols"] = df.shape[1]
+
+    run[f"{base_namespace}/target/n_Healthy"] = df.diagnostic.value_counts()["Healthy"]
+    run[f"{base_namespace}/target/n_SARS-CoV-2"] = df.diagnostic.value_counts()[
+        "SARS-CoV-2"
+    ]
+    run[f"{base_namespace}/target/class_balance"] = neptune.types.File.as_html(
+        px.histogram(df.diagnostic)
+    )
+
+    df.head(n=30).to_csv("data_sample.csv")
+    run[f"{base_namespace}/sample"].upload("data_sample.csv")
+
+
+def log_dataset(
+    run: neptune.Run,
+    base_namespace: str,
+    data: pd.DataFrame,
+    target: pd.DataFrame,
+):
+    run[f"{base_namespace}/n_rows"] = data.shape[0]
+    run[f"{base_namespace}/n_cols"] = data.shape[1]
+
+    run[f"{base_namespace}/target/n_Healthy"] = target.value_counts()[0]
+    run[f"{base_namespace}/target/n_SARS-CoV-2"] = target.value_counts()[1]
+    run[f"{base_namespace}/target/class_balance"] = neptune.types.File.as_html(
+        px.histogram(target)
+    )
+
+
+def log_training_report(run: neptune.Run, base_namespace: str, y_data: zip):
+    for dataset, y_pair in zip(["train", "valid", "test"], y_data):
+        run[f"{base_namespace}/{dataset}/precision"] = precision_score(
+            y_pair[0], y_pair[1]
+        )
+        run[f"{base_namespace}/{dataset}/accuracy"] = accuracy_score(
+            y_pair[0], y_pair[1]
+        )
+        run[f"{base_namespace}/{dataset}/recall"] = recall_score(y_pair[0], y_pair[1])
+
+
+def log_pca(run: neptune.Run, base_namespace: str, pca: sklearn.decomposition.PCA):
+    run[f"{base_namespace}/explained_variance_ratio"].log(
+        list(pca.explained_variance_ratio_)
+    )
+    run[f"{base_namespace}/singular_values"].log(list(pca.singular_values_))
+
+    exp_var = np.cumsum(pca.explained_variance_ratio_)
+
+    fig = px.area(
+        x=range(1, exp_var.shape[0] + 1),
+        y=exp_var,
+        labels={"x": "# Components", "y": "Explained Variance"},
+    )
+
+    run[f"{base_namespace}/explained_variance_chart"] = neptune.types.File.as_html(fig)
